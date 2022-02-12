74f9034c79b136a309d39842071a62365c2d39ae
unknown <PC@admin52.cmd.emaa>
2022-02-12 10:57:43 +0100

    test commit

--- src/main/java/com/sip/ams/NewprojApplication.java
+++ src/main/java/com/sip/ams/NewprojApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class NewprojApplication {
-
+	//testcommit
 	public static void main(String[] args) {
 		SpringApplication.run(NewprojApplication.class, args);
 	}
